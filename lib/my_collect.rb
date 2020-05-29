a287860086fdad67e439e1cd27ab196f947f4a01
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 19:28:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,3 +2,10 @@
 def my_collect(array)
 
 end
+
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
+
+# returns ["Tim", "Tom", "Jim"]
